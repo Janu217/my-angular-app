42553e09346f64b9c5c67b11a34e7df2b1985840
Janaki <Janu217@gmail.com>
2020-10-22 09:24:04 +0530

    Committed approot reading element with attribute and class name.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  // selector: 'app-root-test',
+  //------------Selector with attribute
+  // selector: '[app-root]',
+  //------------Selector with class name
+  selector: '.app-root',
+  //Inline Template
+  // template:"<div>hi</div>",
+  //---------inline template multiple line
+  template:`<h1> Inline template with multiple lines</h1>
+            <h2> multiple lines </h2>`,
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
